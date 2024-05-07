75401c0959d59a3337dcbf462522ea81c15c8f64
mxy963 <myusfoglu@gmail.com>
2024-05-07 22:35:35 +0300

    Update insert_page

---
+++ insert_page/insert_page/lib/image_info.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ImageInfos extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final Color color;
+
+  const ImageInfos({
+    required this.title,
+    required this.onTap,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.rubik(
+              textStyle: const TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(11),
+                border: Border.all(color: Colors.black, width: 1),
+              ),
+              child: const Icon(Icons.attach_file, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ insert_page/insert_page/lib/text_field.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  final String title;
+  final String hintText;
+
+  const TextFieldWidget({
+    required this.title,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.rubik(
+              textStyle: const TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: TextField(
+              enabled: true,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(11.0),
+                  borderSide: const BorderSide(
+                    color: Colors.black,
+                    width: 1.0,
+                  ),
+                ),
+                contentPadding: const EdgeInsets.all(10.0),
+                hintText: hintText,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
