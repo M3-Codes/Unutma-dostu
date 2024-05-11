0f0c565ae26ea742b6364b27301160d5f46d11e5
Bereket <khaldklamhmd.gmail.com>
2024-05-11 21:21:02 +0300

    update view page

--- main_app/lib/pages/view_page.dart
+++ main_app/lib/pages/view_page.dart
@@ -36,7 +36,7 @@ class ViewPage extends StatelessWidget {
             const readingboxes("kapı Aarksı Askılık", "Yer           "),
             const labelColor("#CarKey"),
             const AgainTime("01", "00", "40"),
-            const DayDate("02", "01", "2023"),
+            const DayDate("02", "01", "2022"),
             //Masafe_H(),
           ],
         ),
