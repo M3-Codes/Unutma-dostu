12ec980984e4dd21e5b9a1017accceda8abac241
Bereket <khaldklamhmd.gmail.com>
2024-06-07 22:54:32 +0300

    Added a reload to the home page

--- main_app/lib/pages/home_page.dart
+++ main_app/lib/pages/home_page.dart
@@ -17,6 +17,7 @@ class _HomePageState extends State<HomePage> {
   final ScrollController _scrollController = ScrollController();
 
   bool _doubleBackToExitPressedOnce = false;
+  Key _scaffoldKey = UniqueKey();
 
   Future<bool> _onWillPop() async {
     if (_doubleBackToExitPressedOnce) {
@@ -40,51 +41,62 @@ class _HomePageState extends State<HomePage> {
     return false;
   }
 
+  Future<void> _refreshPage() async {
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      _scaffoldKey = UniqueKey();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
+        key: _scaffoldKey,
         drawer: const Drawer_h(),
-        body: Column(
-          children: <Widget>[
-            const addbar(),
-            Masafe_H(),
-            Expanded(
-              child: RawScrollbar(
-                thumbColor: const Color(0xFFC1007F),
-                radius: const Radius.circular(5),
-                thickness: 9.0,
-                thumbVisibility: true,
-                controller: _scrollController,
-                child: SingleChildScrollView(
+        body: RefreshIndicator(
+          onRefresh: _refreshPage,
+          child: Column(
+            children: <Widget>[
+              const addbar(),
+              Masafe_H(),
+              Expanded(
+                child: RawScrollbar(
+                  thumbColor: const Color(0xFFC1007F),
+                  radius: const Radius.circular(5),
+                  thickness: 9.0,
+                  thumbVisibility: true,
                   controller: _scrollController,
-                  child: Column(
-                    children: [
-                      for (var rowItems in repeatedRowItems)
-                        Column(
-                          children: [
-                            RowBoxes(rowItems),
-                            const divider(),
-                            Masafe_H(),
-                          ],
-                        ),
-                    ],
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    child: Column(
+                      children: [
+                        for (var rowItems in repeatedRowItems)
+                          Column(
+                            children: [
+                              RowBoxes(rowItems),
+                              const divider(),
+                              Masafe_H(),
+                            ],
+                          ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const addbutton(),
-            Masafe_H(),
-            Container(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                "images/yslogo.png",
-                width: 390,
-                height: 65,
+              const addbutton(),
+              Masafe_H(),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  "images/yslogo.png",
+                  width: 390,
+                  height: 65,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
