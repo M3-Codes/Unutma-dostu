52857efaefbdaead033c9af9e831cedbcd230e76
Bereket <khaldklamhmd.gmail.com>
2024-06-08 19:46:09 +0300

    update

--- main_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ main_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import awesome_notifications
 import cloud_firestore
+import connectivity_plus
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -19,6 +20,7 @@ import rive_common
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
